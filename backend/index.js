a16b1af7a5ead2df6f07fa0ef7862c86ac91d458
nhitran <97695039+nhitran26197@users.noreply.github.com>
2023-01-29 10:12:14 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dbConnect = require("./connectMongoDB");
 const User = require("./userTable");
+const PORT = process.env.PORT || 5000
 
 dbConnect();
 
@@ -70,6 +71,6 @@ app.get("/api/leaderboard", (request, response) => {
 
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 3000");
 });
