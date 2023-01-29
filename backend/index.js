cb76e69f8286894084fd24137acab7903fc30c89
nhitran26197 <nhitran26197@gmail.com>
2023-01-28 23:38:07 -0600

    working on leaderboard endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,99 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dbConnect = require("./connectMongoDB");
+const User = require("./userTable");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+dbConnect();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/register", (request, response) => {
+  const user = new User({
+    username: request.body.username,
+    score: request.body.score || 0,
+  });
+  response.json(user);
+
+  User.findOne({ username: request.body.username })
+    .then((result) => {
+      if (result) {
+        console.log("already registered");
+        return response.status(200).send({
+          message: "old user",
+        });
+      } else {
+        console.log("New user");
+        //console.log(user);
+        user
+          .save()
+          .then((savedUser) => {
+            response.status(200).send({
+              message: "New user saved to MongoDB",
+            });
+          })
+          .catch((e) => {
+            response.status(404).send({
+              message: "New user was not saved",
+            });
+          });
+      }
+    })
+    .catch((error) => {
+      response.status(404).send({
+        message: "Can not register",
+        error,
+      });
+    });
+});
+
+app.get("/api/leaderboard", (request, response) => {
+  let arr = [];
+  User.find({})
+    .then((result) => {
+      result.forEach((note) => {
+        arr.push(note.score);
+      });
+      console.log("fkkkk");
+    })
+    .then(() => {
+      arr.sort(function (a, b) {
+        return a - b;
+      });
+      arr.reverse();
+
+      let score = [];
+      let leaderboard = [];
+      const run = async function () {
+        for (let i = 0; i < 10; i++) {
+          score.push(arr[i]);
+          User.findOne({ score: arr[i] }).then((result) => {
+            leaderboard.push(result);
+          });
+        }
+      };
+      run();
+
+      // console.log(typeof score);
+      // console.log("aaaa");
+
+      // console.log(score);
+      // console.log(typeof leaderboard);
+      // console.log(leaderboard);
+      // response.send(leaderboard);
+    });
+  // .catch((e) => {
+  //   console.log("error");
+  //   response.status(404).send({
+  //     message: "Can add notes to the array",
+  //   });
+  // });
+  response.send(leaderboard);
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
-    }
-);
+  console.log("Listening on port 3000");
+});
